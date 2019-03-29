65445457ac2b8ba32dd8bcd4adcf16d01b918bc2
Jens Fischer <soth@gmx.net>
2019-03-29 09:17:08 +0100

    final polishing

--- src/api/BeyondDataSource.js
+++ src/api/BeyondDataSource.js
@@ -1,29 +1,33 @@
-const { RESTDataSource } = require('apollo-datasource-rest');
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class BeyondDataSource extends RESTDataSource {
-    get baseURL() {
-        const api = this.context.beyond_api;
-        const tenant = this.context.hostname.split('.')[0];
-        return api || `https://${tenant}.beyondshop.cloud/api`;
-    }
+  get baseURL() {
+    const api = this.context.beyond_api;
+    const tenant = this.context.hostname.split(".")[0];
+    return api || `https://${tenant}.beyondshop.cloud/api`;
+  }
 
-    willSendRequest(request) {
-        request.headers.set('X-B3-TraceId', this.context.trace_id);
-        request.headers.set('User-Agent', this.context.user_agent);
-        if (this.context.authorization) {
-            request.headers.set('Authorization', this.context.authorization);
-        }
+  willSendRequest(request) {
+    request.headers.set("X-B3-TraceId", this.context.trace_id);
+    request.headers.set("User-Agent", this.context.user_agent);
+    if (this.context.authorization) {
+      request.headers.set("Authorization", this.context.authorization);
     }
+  }
 
-    parseBody(response) {
-        const contentType = response.headers.get('Content-Type');
-        // fix https://github.com/apollographql/apollo-server/blob/master/packages/apollo-datasource-rest/src/RESTDataSource.ts#L107
-        if (contentType && (contentType.startsWith('application/hal+json') || contentType.startsWith('application/json'))) {
-            return response.json();
-        } else {
-            return response.text();
-        }
+  parseBody(response) {
+    const contentType = response.headers.get("Content-Type");
+    // fix https://github.com/apollographql/apollo-server/blob/master/packages/apollo-datasource-rest/src/RESTDataSource.ts#L107
+    if (
+      contentType &&
+      (contentType.startsWith("application/hal+json") ||
+        contentType.startsWith("application/json"))
+    ) {
+      return response.json();
+    } else {
+      return response.text();
     }
-};
+  }
+}
 
 module.exports = { BeyondDataSource };
--- src/api/shop/datasource.js
+++ src/api/shop/datasource.js
@@ -1,51 +1,35 @@
-const { BeyondDataSource } = require('../BeyondDataSource');
+const { BeyondDataSource } = require("../BeyondDataSource");
 
 const cacheOptions = { cacheOptions: { ttl: 120 } };
 
-const createLink = (link) => (link ? {
-    href: link.href,
-    templated: link.templated ? true : false,
-} : {});
+const createLink = link =>
+  link
+    ? {
+        href: link.href,
+        templated: link.templated ? true : false
+      }
+    : {};
 
 class ShopAPI extends BeyondDataSource {
-    async getShop() {
-        return this.get('shop');
-    }
+  async getShop() {
+    return this.get("shop");
+  }
 
-    async getAttributes() {
-        const response = await this.get("shop/attributes", undefined, cacheOptions);
-        return response._embedded ? response._embedded.attributes : [];
-    }
+  async getAttributes() {
+    const response = await this.get("shop/attributes", undefined, cacheOptions);
+    return response._embedded ? response._embedded.attributes : [];
+  }
 
-    async getImages() {
-        const response = await this.get("shop/images", undefined, cacheOptions);
-        const images = response._embedded ? response._embedded.images : [];
-        return images.map(image => ({
-            _id: image._id,
-            label: image.label,
-            data: createLink(image._links.data),
-            metadadata: createLink(image._links.metadadata),
-        }));
-    }
-
-    async getLegalContent(type) {
-        const response = await this.get(`legal-content/${type}`, undefined, cacheOptions);
-        return {
-            type: response.type,
-            content: response.content,
-            mandatory: response.mandatory,
-            pdf: (response._links.pdf ? createLink(response._links.pdf) : null),
-        };
-    }
-
-    async getAllLegalContent(types) {
-        const legalContent = [];
-        for (const type of types) {
-            let response = await this.getLegalContent(type);
-            legalContent.push(response);
-        }
-        return legalContent;
-    }
+  async getImages() {
+    const response = await this.get("shop/images", undefined, cacheOptions);
+    const images = response._embedded ? response._embedded.images : [];
+    return images.map(image => ({
+      _id: image._id,
+      label: image.label,
+      data: createLink(image._links.data),
+      metadadata: createLink(image._links.metadadata)
+    }));
+  }
 }
 
 module.exports = { ShopAPI };
